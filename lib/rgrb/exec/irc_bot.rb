a5846940c49dbdbbc76c2239cd7a0f1582a9af18
ocha <ochaochaocha3@gmail.com>
2014-09-01 21:06:24 +0900

    設定ファイル指定オプションを実装
    fixes #218

--- lib/rgrb/exec/irc_bot.rb
+++ lib/rgrb/exec/irc_bot.rb
@@ -1,9 +1,10 @@
 # vim: fileencoding=utf-8
 
-require 'redis'
 require 'cinch'
+require 'optparse'
 require 'sysexits'
 
+require 'rgrb/version'
 require 'rgrb/config'
 require 'rgrb/plugin/keyword'
 require 'rgrb/plugin/dice_roll'
@@ -13,19 +14,25 @@ module RGRB
   module Exec
     # IRC ボットの実行ファイルの処理を担うクラス
     class IRCBot
+      # 設定ファイルの標準パス（相対パス）
+      DEFAULT_CONFIG_PATH = 'config/rgrb.yaml'
+
       # 新しい RGRB::Exec::IRCBot インスタンスを返す
       # @param [String] rgrb_root_path RGRB のルートディレクトリの絶対パス
       # @param [Array<String>] argv コマンドライン引数の配列
       def initialize(rgrb_root_path, argv)
         @root_path = rgrb_root_path
         @argv = argv
+
+        @config_path = "#{@root_path}/#{DEFAULT_CONFIG_PATH}"
+        @opt = new_opt_parser
       end
 
       # プログラムを実行する
       # @return [void]
       def execute
+        @opt.parse!(@argv)
         load_config
-        prepare_redis_client
         bot = new_bot
 
         # シグナルを捕捉し、ボットを終了させる処理
@@ -35,7 +42,6 @@ module RGRB
           Signal.trap(signal) do
             Thread.new do
               bot.quit
-              @redis.flushdb
             end
           end
         end
@@ -43,40 +49,24 @@ module RGRB
         bot.start
       end
 
-      private
-
       # エラーメッセージを表示する
       # @param [String] message エラーメッセージ
       # @return [void]
       def print_error(message)
-        $stderr.puts("#{File.basename($PROGRAM_NAME, '.*')}: #{message}")
+        $stderr.puts("#{@opt.program_name}: #{message}")
       end
+      private :print_error
 
       # 設定を読み込む
       # @return [void]
       # @todo ファイル名を指定して読み込めるようにする
       def load_config
-        yaml_path = "#{@root_path}/config/rgrb.yaml"
-        @config = RGRB::Config.load_yaml_file(yaml_path)
+        @config = RGRB::Config.load_yaml_file(@config_path)
       rescue => e
         print_error("設定ファイルの読み込みに失敗しました (#{e})")
         Sysexits.exit(:config_error)
       end
-
-      # Redis クライアントを準備する
-      # @return [void]
-      def prepare_redis_client
-        redis_config = @config.redis
-        @redis = Redis.new(
-          host: redis_config['Host'],
-          port: redis_config['Port'],
-          db: redis_config['Database']
-        )
-        @redis.flushdb
-      rescue => e
-        print_error("Redis クライアントの生成に失敗しました (#{e})")
-        $stderr.puts('再度設定を確認してください')
-      end
+      private :load_config
 
       # IRC ボットを作り、設定して返す
       # @return [Cinch::Bot]
@@ -101,7 +91,6 @@ module RGRB
                 plugin_class,
                 {
                   rgrb_root_path: @root_path,
-                  redis: @redis
                 }
               ]
             end
@@ -114,6 +103,34 @@ module RGRB
         $stderr.puts('再度設定を確認してください')
         Sysexits.exit(:config_error)
       end
+      private :new_bot
+
+      # 新しい OptionParser を作り、設定して返す
+      # @return [OptionParser]
+      def new_opt_parser
+        OptionParser.new do |opt|
+          opt.summary_indent = ' ' * 2
+          opt.summary_width = 24
+          opt.banner = <<EOS
+使用法: #{opt.program_name} [オプション]
+
+汎用ダイスボット RGRB - IRC ボット
+EOS
+          opt.version = RGRB::VERSION
+
+          opt.separator('')
+          opt.separator('オプション:')
+
+          opt.on(
+            '-c',
+            '--config=CONFIG_FILE',
+            '設定ファイルとして CONFIG_FILE を読み込みます'
+          ) do |config_file|
+            @config_path = config_file
+          end
+        end
+      end
+      private :new_opt_parser
     end
   end
 end
---
+++ lib/rgrb/version.rb
@@ -0,0 +1,6 @@
+# vim: fileencoding=utf-8
+
+module RGRB
+  # RGRB のバージョン
+  VERSION = '0.2.0'
+end

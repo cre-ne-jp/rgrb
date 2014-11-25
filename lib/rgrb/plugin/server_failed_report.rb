1675c9a8d1cb79d411529bba275566ff5c7c5dcc
koi-chan <koi-chan@mail.kazagakure.net>
2014-11-25 21:30:22 +0900

    プラグイン ServerFailedReport 新規作成
    チャンネル &SERVER へ自動的にあらかじめJOINし、そのログを読み取ってトリガーとする。
    将来的には cre_twitter_citation プラグインと同様に、設定ファイルで報告先のチャンネルを指定できるようにする。

---
+++ lib/rgrb/plugin/server_failed_report.rb
@@ -0,0 +1,37 @@
+# vim: fileencoding=utf-8
+
+require 'cinch'
+
+module RGRB
+  module Plugin
+    # キーワード検索プラグイン
+    class ServerFailedReport
+      include Cinch::Plugin
+
+      # サーバーがリレーから切断した際のメッセージ
+      FAIL_MESSAGE = '"%s"がリレーから切り離されました。'
+      # サーバーがリレーした際のメッセージ
+      CONNECT_MESSAGE = '"%s"がリレーしました。'
+
+			# メッセージ出力先
+			@channel_name = ''
+
+      match(/Server "([\w\.]+)" unregistered/, method: :fail_srv)
+      match(/Server "([\w\.]+)" registered/, method: :connect_srv)
+
+			# NOTICE でサーバー切断メッセージを出す
+      # @return [void]
+      def fail_srv(m, srvname)
+				mes = FAIL_MESSAGE % srvname
+				Channel(@channel_name).safe_notice(mes)
+      end
+
+      # NOTICE でサーバー接続メッセージを返す
+      # @return [void]
+      def connect_srv(m, srvname)
+				mes = CONNECT_MESSAGE % srvname
+				Channel(@channel_name).safe_notice(mes)
+      end
+    end
+  end
+end

44128a01ab4ccc46d157c358b63801d9b9ff0f95
koi-chan <koi-chan@mail.kazagakure.net>
2014-12-08 21:53:39 +0900

    Plugin "ServerFailedReport" を追加。
    まだうまく動かないが、データを逃がすためにもコミットしておく。
    
    現在、考えている完成形
    サーバーの動作ログが自動的に流される特殊なチャンネル &SERVER にJOINしておく。
    そこからサーバーがリレーから切り離されたとき・加わった時のログを検知し、設定で指定したチャンネルにメッセージを流す。

--- lib/rgrb/plugin/server_failed_report.rb
+++ lib/rgrb/plugin/server_failed_report.rb
@@ -18,19 +18,29 @@ module RGRB
 
       match(/Server "([\w\.]+)" unregistered/, method: :fail_srv)
       match(/Server "([\w\.]+)" registered/, method: :connect_srv)
+			match(/roll[ 　]+([1-9]\d*)d([1-9]\d*)/i, method: :basic_dice)
+
+			def basic_dice(m, n_dice, max)
+			  m.target.notice("#{m.user.nick} -> #{n_dice}d#{max} = test")
+			end
+
 
 			# NOTICE でサーバー切断メッセージを出す
       # @return [void]
       def fail_srv(m, srvname)
 				mes = FAIL_MESSAGE % srvname
-				Channel(@channel_name).safe_notice(mes)
+#				@channel_name.each do |channel_name|
+					Channel(@channel_name).safe_notice(mes)
+#				end
       end
 
       # NOTICE でサーバー接続メッセージを返す
       # @return [void]
       def connect_srv(m, srvname)
 				mes = CONNECT_MESSAGE % srvname
-				Channel(@channel_name).safe_notice(mes)
+#				@channel_name.each do |channel_name|
+					Channel(@channel_name).safe_notice(mes)
+#				end
       end
     end
   end

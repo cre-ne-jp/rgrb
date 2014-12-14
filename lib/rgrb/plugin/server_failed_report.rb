ffc1c6b2cf41a14f3e8c4a3430a86270c2a30830
koi-chan <koi-chan@mail.kazagakure.net>
2014-12-14 15:43:34 +0900

    プラグイン ServerFailedReport の修正。
    サーバーのログチャンネル &SERVER からの発言のみをトリガーとしてサーバー切断・接続メッセージを発するように変更。
    今までは、チャンネル・発言者に関わらず特定のメッセージに反応してアナウンスをしていたが、スパムにつながる危険があった。
    しかし、ngIRCd はサーバー内チャンネル &SERVER にのみログを流し、このチャンネルはサーバーしか発言できないようになっているため、反応するチャンネルを限ることでスパムを防止することが可能になる。

--- lib/rgrb/plugin/server_failed_report.rb
+++ lib/rgrb/plugin/server_failed_report.rb
@@ -13,35 +13,38 @@ module RGRB
       # サーバーがリレーした際のメッセージ
       CONNECT_MESSAGE = '"%s"がリレーしました。'
 
-			# メッセージ出力先
-			#@channels_to_send = ''
+      # メッセージ出力先
+      @channels_to_send = ''
 
-			self.prefix = ''
+      self.prefix = ''
       match(/Server "([\w\.]+)" unregistered/, method: :fail_srv)
       match(/Server "([\w\.]+)" registered/, method: :connect_srv)
 
-			def initialize(*args)
-				super
+      def initialize(*args)
+        super
+        plugin_config = config[:rgrb_config].plugin_config(self.class)
 
-				load_config
-			end
+        @channels_to_send = plugin_config['ChannelsToSend']
+      end
 
-			# NOTICE でサーバー切断メッセージを出す
+      # NOTICE でサーバー切断メッセージを出す
       # @return [void]
       def fail_srv(m, srvname)
-				mes = FAIL_MESSAGE % srvname
-				@channels_to_send.each do |channel_name|
-					Channel(channel_name).safe_notice(mes)
-				end
+        return unless m.channel == '&SERVER'
+        mes = FAIL_MESSAGE % srvname
+        @channels_to_send.each do |channel_name|
+          Channel(channel_name).safe_notice(mes)
+        end
       end
 
       # NOTICE でサーバー接続メッセージを返す
       # @return [void]
       def connect_srv(m, srvname)
-				mes = CONNECT_MESSAGE % srvname
-				@channels_to_send.each do |channel_name|
-					Channel(channel_name).safe_notice(mes)
-				end
+        return unless m.channel == '&SERVER'
+        mes = CONNECT_MESSAGE % srvname
+        @channels_to_send.each do |channel_name|
+          Channel(channel_name).safe_notice(mes)
+        end
       end
     end
   end

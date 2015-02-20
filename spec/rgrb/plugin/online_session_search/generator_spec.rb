1b1335d95a4f21b7ea739b21f6d28464dfc625a2
ocha <ochaochaocha3@gmail.com>
2015-02-20 23:35:26 +0900

    ons: webmock を追加
    refs #6

--- spec/rgrb/plugin/online_session_search/generator_spec.rb
+++ spec/rgrb/plugin/online_session_search/generator_spec.rb
@@ -1,7 +1,7 @@
 # vim: fileencoding=utf-8
 
+require 'open-uri'
 require_relative '../../../spec_helper'
-require 'webmock/rspec'
 require 'rgrb/plugin/online_session_search/generator'
 require 'rgrb/plugin/online_session_search/session'
 
@@ -83,33 +83,95 @@ describe RGRB::Plugin::OnlineSessionSearch::Generator do
     end
   end
 
-  describe '#latest_schedules' do
-    context 'セッション情報が見つからなかったとき' do
+  describe '#session_data_from (private)' do
+    context 'HTTP エラーが起きたとき' do
+      let(:url) { 'http://session.trpg.net/json.php?n=5' }
+
       before do
-        stub_request(:get, 'http://session.trpg.net/json.php?n=5').
-          to_return(body: '[]')
+        stub_request(:get, url).to_raise(StandardError)
       end
 
-      subject { generator.latest_schedules(5) }
+      it do
+        expect { generator.send(:session_data_from, url) }.to raise_error
+      end
+    end
+
+    context 'セッション情報が見つからなかったとき' do
+      let(:url) { 'http://session.trpg.net/json.php?n=5' }
+
+      before do
+        stub_request(:get, url).to_return(body: '[]')
+      end
+
+      subject { generator.send(:session_data_from, url) }
       it { should eq(['開催予定のセッションは見つかりませんでしたの☆']) }
     end
 
     context 'セッション 1, 2' do
+      let(:url) { 'http://session.trpg.net/json.php?n=2' }
       let(:session_1_2_json) do
         '[{"SID":"9999","account":"@foo","username":"Foo","twitterimage":"http:\/\/example.net\/foo.png","SesName":"セッション 1","StartTime":"2015-01-23 04:56:43","MinMembers":"3","MaxMembers":"4","SysName":"ソードワールド2.0","url":"http:\/\/session.trpg.net\/9999"},{"SID":"99999","account":"@bar","username":"Bar","twitterimage":"http:\/\/example.org\/bar.png","SesName":"セッション 2","StartTime":"2015-02-10 23:59:59","MinMembers":"5","MaxMembers":"5","SysName":"エリュシオン","url":"http:\/\/session.trpg.net\/99999"}]'
       end
 
       before do
-        stub_request(:get, 'http://session.trpg.net/json.php?n=5').
-          to_return(body: session_1_2_json)
+        stub_request(:get, url).to_return(body: session_1_2_json)
       end
 
-      subject { generator.latest_schedules(5) }
+      subject { generator.send(:session_data_from, url) }
       it do
         should eq([
           'ソードワールド2.0 / セッション 1 (2015-01-23 04:56; 3-4人; http://session.trpg.net/9999)',
           'エリュシオン / セッション 2 (2015-02-10 23:59; 5人; http://session.trpg.net/99999)'
         ])
+      end
+    end
+  end
+
+  describe '#latest_schedules' do
+    shared_examples 'latest_schedules' do |num|
+      context "n = #{num}" do
+        let(:url) { "http://session.trpg.net/json.php?n=#{num}" }
+
+        before do
+          stub_request(:get, url).to_return(body: '[]')
+        end
+
+        it '正しい URL に対して GET を送る' do
+          generator.latest_schedules(num)
+          expect(WebMock).to have_requested(:get, url).once
+        end
+      end
+    end
+
+    include_examples 'latest_schedules', 5
+    include_examples 'latest_schedules', 10
+  end
+
+  describe '#search' do
+    shared_examples 'search' do
+      before do
+        stub_request(:get, url).to_return(body: '[]')
+      end
+
+      it '正しい URL に対して GET を送る' do
+        generator.search(str, num)
+        expect(WebMock).to have_requested(:get, url).once
+      end
+    end
+
+    context 'ソードワールド (n = 5)' do
+      include_examples 'search' do
+        let(:str) { 'ソードワールド' }
+        let(:num) { 5 }
+        let(:url) { 'http://session.trpg.net/json.php?s=%E3%82%BD%E3%83%BC%E3%83%89%E3%83%AF%E3%83%BC%E3%83%AB%E3%83%89&n=5' }
+      end
+    end
+
+    context 'エリュシオン (n = 10)' do
+      include_examples 'search' do
+        let(:str) { 'エリュシオン' }
+        let(:num) { 10 }
+        let(:url) { 'http://session.trpg.net/json.php?s=%E3%82%A8%E3%83%AA%E3%83%A5%E3%82%B7%E3%82%AA%E3%83%B3&n=10' }
       end
     end
   end

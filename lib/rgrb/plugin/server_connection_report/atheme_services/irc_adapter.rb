25f94845ac3713424242c799328187902abbd1dd
koi-chan <koi-chan@mail.kazagakure.net>
2015-05-08 10:05:53 +0900

    ServerConnectionReport: Atheme-Services のサポート
    Atheme-Services が出力するサーバー状態を検知することができるようになった。

---
+++ lib/rgrb/plugin/server_connection_report/atheme_services/irc_adapter.rb
@@ -0,0 +1,65 @@
+# vim: fileencoding=utf-8
+
+require 'cinch'
+
+require 'rgrb/plugin/util/notice_on_each_channel'
+require 'rgrb/plugin/util/logging'
+require 'rgrb/plugin/server_connection_report/generator'
+
+module RGRB
+  module Plugin
+    module ServerConnectionReport
+      # Atheme-Services 用サーバリレー監視プラグイン
+      module AthemeServices
+        # ServerConnectionReport::AthemeServices の IRC アダプター
+        class IrcAdapter
+          include Cinch::Plugin
+          include Util::NoticeOnEachChannel
+
+          # メッセージを送信するチャンネルのリスト
+          attr_reader :channels_to_send
+
+          # サーバメッセージのチャンネル名
+          SERVER_MESSAGE_CHANNEL = '#services'
+
+          set(plugin_name: 'ServerConnectionReport::AthemeServices')
+          self.prefix = ''
+
+          match(/^server_add\(\): ([\w\.]+)/, method: :joined)
+          match(/^server_delete\(\): ([\w\.]+)/, method: :disconnected)
+
+          def initialize(*)
+            super
+
+            config_data = config[:plugin]
+            @channels_to_send = config_data['ChannelsToSend']
+
+            @generator = Generator.new
+          end
+
+          # サーバ接続メッセージを NOTICE する
+          # @param [Cinch::Message] m メッセージ
+          # @param [String] server サーバ
+          # @return [void]
+          def joined(m, server)
+            if m.channel == SERVER_MESSAGE_CHANNEL
+              log_incoming(m)
+              notice_on_each_channel(@generator.joined(server))
+            end
+          end
+
+          # サーバ切断メッセージを NOTICE する
+          # @param [Cinch::Message] m メッセージ
+          # @param [String] server サーバ
+          # @return [void]
+          def disconnected(m, server)
+            if m.channel == SERVER_MESSAGE_CHANNEL
+              log_incoming(m)
+              notice_on_each_channel(@generator.disconnected(server))
+            end
+          end
+        end
+      end
+    end
+  end
+end

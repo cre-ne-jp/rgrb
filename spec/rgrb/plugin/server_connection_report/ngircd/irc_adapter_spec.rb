2644c51a2495e663f5a94c4cdb48342491fae79e
ocha <ochaochaocha3@gmail.com>
2015-06-18 22:35:19 +0900

    ServerConnectionReport: UNREGISTERED_RE のテストを追加する
    refs #47

--- spec/rgrb/plugin/server_connection_report/ngircd/irc_adapter_spec.rb
+++ spec/rgrb/plugin/server_connection_report/ngircd/irc_adapter_spec.rb
@@ -4,6 +4,17 @@ require_relative '../../../../spec_helper'
 require 'rgrb/plugin/server_connection_report/ngircd/irc_adapter'
 
 describe RGRB::Plugin::ServerConnectionReport::Ngircd::IrcAdapter do
+  servers = [
+    'irc.cre.jp',
+    'irc.cre.ne.jp',
+    'irc.egotex.net',
+    'irc.kazagakure.net',
+    'irc.r-roman.net',
+    'irc.sougetu.net',
+    'irc.trpg.net',
+    't-net.xyz'
+  ]
+
   describe 'REGISTERED_RE' do
     shared_examples 'registered' do |server|
       context(server) do
@@ -20,15 +31,29 @@ describe RGRB::Plugin::ServerConnectionReport::Ngircd::IrcAdapter do
       end
     end
 
-    servers = [
-      'irc.cre.ne.jp',
-      'irc.kazagakure.net',
-      't-net.xyz',
-      'irc.r-roman.net'
-    ]
-
     servers.each do |server|
       include_examples 'registered', server
+    end
+  end
+
+  describe 'UNREGISTERED_RE' do
+    shared_examples 'unregistered' do |server|
+      context(server) do
+        let(:message) { %!Server "#{server}" unregistered: foo.! }
+
+        it 'マッチする' do
+          expect(message).to match(described_class::UNREGISTERED_RE)
+        end
+
+        it 'ホスト名を抜き出せる' do
+          m = message.match(described_class::UNREGISTERED_RE)
+          expect(m[1]).to eq(server)
+        end
+      end
+    end
+
+    servers.each do |server|
+      include_examples 'unregistered', server
     end
   end
 end

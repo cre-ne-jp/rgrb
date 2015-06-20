e9fcb8814c8ec904c9c74e905c187bc1dc9db2f0
ocha <ochaochaocha3@gmail.com>
2015-06-21 00:05:01 +0900

    ServerConnectionReport: ホスト名のマッチ対象を広げる
    fixes #49

--- lib/rgrb/plugin/server_connection_report/constants.rb
+++ lib/rgrb/plugin/server_connection_report/constants.rb
@@ -4,7 +4,9 @@ module RGRB
   module Plugin
     module ServerConnectionReport
       # ホスト名を表す正規表現
-      HOSTNAME_RE = /(?:[\w\.]+)/
+      HOSTNAME_RE =
+        /(?:[a-z\d](?:[-a-z\d]{0,61}[a-z\d])?
+          (?:\.[a-z\d](?:[-a-z\d]{0,61}[a-z\d])?)*)/ix
     end
   end
 end

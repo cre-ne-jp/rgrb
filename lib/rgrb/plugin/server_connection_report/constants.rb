75ffe3579e7a42c9eaccfd6b4851aef67509c49d
ocha <ochaochaocha3@gmail.com>
2015-06-21 00:10:28 +0900

    ServerConnectionReport: @see を追加する
    refs #49
    [ci skip]

--- lib/rgrb/plugin/server_connection_report/constants.rb
+++ lib/rgrb/plugin/server_connection_report/constants.rb
@@ -4,6 +4,8 @@ module RGRB
   module Plugin
     module ServerConnectionReport
       # ホスト名を表す正規表現
+      #
+      # @see http://stackoverflow.com/questions/106179/regular-expression-to-match-dns-hostname-or-ip-address
       HOSTNAME_RE =
         /(?:[a-z\d](?:[-a-z\d]{0,61}[a-z\d])?
           (?:\.[a-z\d](?:[-a-z\d]{0,61}[a-z\d])?)*)/ix

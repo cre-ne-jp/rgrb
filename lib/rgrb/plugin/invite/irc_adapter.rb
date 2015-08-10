58b7cbd7c05740ed2d7ec1c2e6fbfbc91ac369b1
koi-chan <koi-chan@mail.kazagakure.net>
2015-08-10 16:43:57 +0900

    Invite: プラグインを新規作成
    JOIN メッセージが複数行でも発言できるようにしてある

---
+++ lib/rgrb/plugin/invite/irc_adapter.rb
@@ -0,0 +1,40 @@
+# vim: fileencoding=utf-8
+
+require 'cinch'
+require 'rgrb/plugin/configurable_adapter'
+
+module RGRB
+  module Plugin
+    # INVITE されたとき、そのチャンネルに JOIN するプラグイン
+    module Invite
+      # Invite の IRC アダプター
+      class IrcAdapter
+        include Cinch::Plugin
+        include ConfigurableAdapter
+
+        set(plugin_name: 'Invite')
+        listen_to(:invite, method: :invite)
+
+        def initialize(*args)
+          super
+
+          config_data = config[:plugin] || {}
+          @join_message =
+            config_data['JoinMessage'] || 'ご招待いただきありがとう'
+        end
+
+        # 自分が INVITE されたら自動的にそのチャンネルに入る
+        # @param [Cinch::Message] m 送信されたメッセージ
+        # @return [void]
+        def invite(m)
+          Channel(m.channel).join
+          @join_message.each do |line|
+            message = line.chomp
+            m.target.send(message, true)
+            log_notice(m.target, message)
+          end
+        end
+      end
+    end
+  end
+end

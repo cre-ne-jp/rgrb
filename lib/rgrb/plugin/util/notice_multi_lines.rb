a16ab1e338c840e2dded5494504d70dc68d3051f
koi-chan <koi-chan@mail.kazagakure.net>
2015-09-20 17:35:24 +0900

    NoticeMultiLines: 表示不可能な文字を排除できるようにした

--- lib/rgrb/plugin/util/notice_multi_lines.rb
+++ lib/rgrb/plugin/util/notice_multi_lines.rb
@@ -15,11 +15,16 @@ module RGRB
         # @param [Array<String>] lines NOTICE するメッセージ
         # @param [Cinch::Target] target NOTICE 先
         # @param [String] header メッセージの先頭に挿入する文字列
+        # @param [Boolean] safe 表示不可能な文字を排除するかどうか
         # @return [void]
-        def notice_multi_lines(lines, target, header = '')
+        def notice_multi_lines(lines, target, header = '', safe = false)
           lines.each do |line|
             message = "#{header}#{line.chomp}"
-            target.send(message, true)
+            if(safe)
+              target.safe_send(message, true)
+            else
+              target.send(message, true)
+            end
             log_notice(target, message)
           end
         end
@@ -28,9 +33,10 @@ module RGRB
         # @param [String] messages 改行文字を含む送信メッセージ
         # @param [Cinch::Target] target NOTICE 先
         # @param [String] header メッセージの先頭に挿入する文字列
+        # @param [Boolean] safe 表示不可能な文字を排除するかどうか
         # @return [void]
-        def notice_multi_messages(messages, target, header = '')
-          notice_multi_lines(messages.split("$/"), target, header)
+        def notice_multi_messages(messages, target, header = '', safe = false)
+          notice_multi_lines(messages.split("$/"), target, header, safe)
         end
       end
     end

cd4a2946baa4cf1877b992f292927470a5ed6997
koi-chan <koi-chan@mail.kazagakure.net>
2017-11-09 08:09:27 +0900

    BCDice ラッパープラグイン実装開始
    * BCDice は現時点での最新版 commit:e30d115 をサブモジュールとして組み込み
    * ダイスコマンド・ゲームシステム名の両方が入力必須
    * TODO
      - ゲームシステム名を省略しても通常ダイスを振れるようにする
      - ダイスコマンドのリファクタリングと、@ 以外に特殊文字を使っていないかの確認
      - マニュアルの用意
      - BCDice のバージョン情報を出力するコマンドの実装
    * 要検討
      - ゲームシステム名を日本語で入力してもダイスが振れるようにする
      - BCDice の多言語化に対応
      - BCDice ヘルプメッセージを出力するようにするか

---
+++ lib/rgrb/plugin/bcdice/irc_adapter.rb
@@ -0,0 +1,40 @@
+# vim: fileencoding=utf-8
+
+require 'cinch'
+require 'rgrb/plugin/util/notice_multi_lines'
+
+require 'rgrb/../../vendor/BCDice/src/cgiDiceBot'
+
+module RGRB
+  module Plugin
+    # BCDice のラッパープラグイン
+    module Bcdice
+      # Bcdice の IRC アダプター
+      class IrcAdapter
+        include Cinch::Plugin
+        include Util::NoticeMultiLines
+
+        set(plugin_name: 'Bcdice')
+        self.prefix = '.bcdice'
+        match(/[ 　]+([A-z0-9@]+)[ 　]+([A-z0-9]+)/, method: :bcdice)
+
+        def initialize(*args)
+          super
+
+          @bcdice = CgiDiceBot.new
+        end
+
+        # BCDice でダイスを振る
+        # @param [Cinch::Message] m 送信されたメッセージ
+        # @param [String] command ダイスコマンド
+        # @param [String] gameType ゲームタイプ
+        # @return [void]
+        def bcdice(m, command, gameType = '')
+          result = @bcdice.roll(command, gameType)
+          message = result[0].lstrip
+          m.target.send(message, true)
+        end
+      end
+    end
+  end
+end

08f83b567db36826a03c5a7cd14b71aed4f1649f
koi-chan <koi-chan@mail.kazagakure.net>
2019-07-21 21:33:02 +0900

    DiceRoll プラグイン: Discord アダプターにシークレットロール機能を実装

--- lib/rgrb/plugin/dice_roll/discord_adapter.rb
+++ lib/rgrb/plugin/dice_roll/discord_adapter.rb
@@ -15,6 +15,9 @@ module RGRB
         self.prefix = /\.roll[\s　]+/
         match(/(#{NUMS_RE})d(#{NUMS_RE})/io, method: :basic_dice)
         match(/d(#{NUM_RE}+)/io, method: :dxx_dice)
+        match(/(#{NUMS_RE})d(#{NUMS_RE})/io, method: :basic_dice_secret, prefix: /\.sroll[\s　]+/)
+        match(/d(#{NUM_RE}+)/io, method: :dxx_dice_secret, prefix: /\.sroll[\s　]+/)
+        match('-open', method: :open_dice, prefix: '.sroll')
 
         match(/(#{KANA_NUMS_RE})の(#{KANA_NUMS_RE})/io, method: :basic_dice_ja, :prefix => '。')
         match(/の(#{KANA_NUM_RE}+)/io, method: :dxx_dice_ja, :prefix => '。')
@@ -33,32 +36,86 @@ module RGRB
         # @return [void]
         def basic_dice(m, n_dice, max)
           log_incoming(m)
-          result = @generator.basic_dice(n_dice.to_i, max.to_i)
-          send_channel(m.channel, "#{m.user.mention} -> #{result}")
+          send_result(m, @generator.basic_dice(n_dice.to_i, max.to_i))
         end
 
+        # 基本的なダイスロールの結果を返す
+        # シークレットロール
+        # @return [void]
+        def basic_dice_secret(m, n_dice, max)
+          log_incoming(m)
+          send_result(m, @generator.basic_dice(n_dice.to_i, max.to_i), true)
+        end
+
+        # 日本語版の basic_dice
+        # @return [void]
         def basic_dice_ja(m, n_dice, max)
           log_incoming(m)
           return unless @jadice
 
-          result = @generator.basic_dice_ja(n_dice, max)
-          send_channel(m.channel, "#{m.user.mention} -> #{result}")
+          send_result(m, @generator.basic_dice_ja(n_dice, max))
         end
 
         # d66 など、出目をそのままつなげるダイスロールの結果を返す
         # @return [void]
         def dxx_dice(m, rolls)
           log_incoming(m)
-          result = @generator.dxx_dice(rolls)
-          send_channel(m.channel, "#{m.user.mention} -> #{result}")
+          send_result(m, @generator.dxx_dice(rolls))
         end
 
+        # d66 など、出目をそのままつなげるダイスロールの結果を返す
+        # シークレットロール
+        # @return [void]
+        def dxx_dice_secret(m, rolls)
+          log_incoming(m)
+          send_result(m, @generator.dxx_dice(rolls), true)
+        end
+
+        # 日本語版の dxx_dice
+        # @return [void]
         def dxx_dice_ja(m, rolls)
           log_incoming(m)
           return unless @jadice
 
-          result = @generator.dxx_dice_ja(rolls)
-          message = "#{m.user.mention} -> #{result}"
+          send_result(m, @generator.dxx_dice_ja(rolls))
+        end
+
+        # シークレットロールを表示する
+        # @return [void]
+        def open_dice(m)
+          result = @generator.open_dice(m.channel.id.to_s)
+          messages = if result.nil?
+              "##{m.channel.name} にはシークレットロール結果がありません"
+            else
+              [
+                "##{m.channel.name} のシークレットロール: #{result.size} 件",
+                result,
+                "シークレットロールここまで"
+              ].flatten
+            end
+          send_channel(m.channel, messages)
+        end
+
+        private
+
+        # シークレットロール結果を保存し、Discord に報告する
+        # @param [] m
+        # @param [String] result ダイスロール結果
+        # @param [Boolean] secret シークレットダイスか？
+        # @option secret true  シークレットダイス
+        # @option secret false オープンダイス
+        # @return [void]
+        def send_result(m, result, secret = false)
+          message = if secret
+              @generator.save_secret_roll(
+                m.channel.id.to_s,
+                "#{m.user.name} -> #{result}"
+              )
+              "#{m.user.mention}: シークレットロールを保存しました"
+            else
+              "#{m.user.mention} -> #{result}"
+            end
+
           send_channel(m.channel, message)
         end
       end

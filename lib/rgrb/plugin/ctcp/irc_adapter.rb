4de0ca510d73652fc86b0a2dd708efb4031e4e1c
ocha <ochaochaocha3@gmail.com>
2019-09-26 22:40:58 +0900

    CTCP: コメントを追加する
    RFCのドラフトにおける各コマンドの説明のURLを追加したかった

--- lib/rgrb/plugin/ctcp/irc_adapter.rb
+++ lib/rgrb/plugin/ctcp/irc_adapter.rb
@@ -22,33 +22,58 @@ module RGRB
           ctcp(command)
         end
 
-        def initialize(*args)
+        # プラグインを初期化する
+        def initialize(*)
           super
 
           config_data = config[:plugin] || {}
           @userinfo = config_data['UserInfo'] || 'RGRB 稼働中'
         end
 
+        # 利用可能なコマンドを返す
+        # @param [Cinch::Message] m 受信したメッセージ
+        # @return [void]
+        # @see https://tools.ietf.org/id/draft-oakley-irc-ctcp-01.html#clientinfo
         def ctcp_clientinfo(m)
           ctcp_reply(m, AVAILABLE_COMMANDS.join(' '))
         end
 
+        # バージョン情報を返す
+        # @param [Cinch::Message] m 受信したメッセージ
+        # @return [void]
+        # @see https://tools.ietf.org/id/draft-oakley-irc-ctcp-01.html#version
         def ctcp_version(m)
           ctcp_reply(m, "RGRB #{RGRB::VERSION_WITH_COMMIT_ID}")
         end
 
+        # 現在のローカル時刻を返す
+        # @param [Cinch::Message] m 受信したメッセージ
+        # @return [void]
+        # @see https://tools.ietf.org/id/draft-oakley-irc-ctcp-01.html#time
         def ctcp_time(m)
           ctcp_reply(m, Time.now.strftime('%a, %d %b %Y %T %z'))
         end
 
+        # クエリと同じパラメータを返す
+        # @param [Cinch::Message] m 受信したメッセージ
+        # @return [void]
+        # @see https://tools.ietf.org/id/draft-oakley-irc-ctcp-01.html#ping
         def ctcp_ping(m)
           ctcp_reply(m, m.ctcp_args.join(' '))
         end
 
+        # ユーザについての情報を返す
+        # @param [Cinch::Message] m 受信したメッセージ
+        # @return [void]
+        # @see https://tools.ietf.org/id/draft-oakley-irc-ctcp-01.html#userinfo
         def ctcp_userinfo(m)
           ctcp_reply(m, @userinfo)
         end
 
+        # RGRB のソースコードが存在する場所を返す
+        # @param [Cinch::Message] m 受信したメッセージ
+        # @return [void]
+        # @see https://tools.ietf.org/id/draft-oakley-irc-ctcp-01.html#source
         def ctcp_source(m)
           ctcp_reply(m, 'https://github.com/cre-ne-jp/rgrb')
         end
@@ -56,8 +81,8 @@ module RGRB
         private
 
         # CTCP 応答を返す
-        # @param [Cinch::Message] m
-        # @param [String] message 送信メッセージ
+        # @param [Cinch::Message] m 受信したメッセージ
+        # @param [String] message 送信するメッセージ
         # @return [void]
         def ctcp_reply(m, message)
           log_incoming(m)

05b4b4d6c33e6bc6ca76b91bb029dc886dac8873
koi-chan <koi-chan@mail.kazagakure.net>
2020-05-30 00:00:02 +0900

    keyword: キーワード検索の URL を、ログアーカイブのキーワード検索ページに変更

--- lib/rgrb/plugin/keyword/generator.rb
+++ lib/rgrb/plugin/keyword/generator.rb
@@ -12,7 +12,8 @@ module RGRB
         include PluginBase::Generator
 
         # cre.jp 検索ページの URL
-        CRE_SEARCH_URL = 'http://cre.jp/search/?sw=%s'
+        # %s が与えられたキーワードを URL エンコードした物に置換される
+        CRE_SEARCH_URL = 'https://log.irc.cre.jp/keywords/%s'
 
         # 新しい Keyword::Generator インスタンスを返す
         def initialize
